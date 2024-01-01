9088faa71770171562a1ca9e5bbb58b8b9b0aa38
SuperZekes <149198602+SuperZekes@users.noreply.github.com>
2024-01-01 13:28:56 -0400

    Create modlist.java

---
+++ modlist.java
@@ -0,0 +1,54 @@
+3D Skin Layers
+BetterF3
+Better Third Person
+Debugify
+ImmediatelyFast
+No Chat Reports
+Xaero's World Map
+Xaero's Minimap
+Zoomify
+AppleSkin
+BetterHurtCam
+Better Statistics Screen
+Capes
+Chat Coords
+Chat Heads
+Chat Animation
+Cloth Config API
+Dark Loading Screen
+Dynamic FPS
+Entity Culling
+Fabric API
+Fabric Language Kotlin
+FerriteCore
+Indium
+Inventory Management
+Iris Shaders
+libIPN
+Lithium
+ModernFix
+Mod Menu
+More Chat History
+More Culling
+Not Enough Animations
+Reese's Sodium Options
+Shulker Box Tooltip
+Simple Armor Hud
+Totem Small pop
+Sodium Extra
+Sodium
+ThreadTweak
+TotemCounter
+ukulib
+ViaFabricPlus
+Visuality
+Very Many Players (Fabric)
+Simple Voice Chat
+World Host
+YetAnotherConfigLib
+Your Options Shall Be Respected (YOSBR)
+Chat Reporting Helper
+Default Dark Mode
+Fast Better Grass
+Translations for Sodium
+cubic-sun-moon-v1-7-1a.zip
